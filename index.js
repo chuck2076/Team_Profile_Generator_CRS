b9036b53b2d186a9a97a3ee52e87f8062ddcad9e
Chuck Stephens <chuckstephens2076@gmail.com>
2021-10-16 16:11:17 -0500

    Got everything to console log!

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const generateHTML = require("./src/generateHTML")
+
 const inquirer = require("inquirer");
 const fs = require("fs");
 const Employee = require("./lib/employee");
@@ -5,37 +7,97 @@ const Manager = require("./lib/manager");
 const Intern = require("./lib/intern");
 const Engineer = require("./lib/engineer");
 
-const questions = [{
-    type: "list",
-    name: "role",
-    message: "What is the role of the employee?",
-    choices: ["Manager", "Engineer", "Intern"]
-    },
+const teamArray = [];
+
+const manQ = () => {
+return inquirer.prompt ([
 {
-        type: "input",
-        name: "name",
-        message: "Name of employee:"
-    },
+    type: "input",
+    name: "name",
+    message: "Name of Team Manager?"
+
+},
+
 {
     type: "input",
     name: "id",
-    message: "What is the employee ID:",
-
+    message: "What is the Manager ID?",
+    validate: nameInput => {
+    if (isNaN(nameInput)) {
+        console.log ("Please enter a Number!")
+        return false;
+    }else {
+        return true;
+    }
+  }
 },
 
 {
     type: "input",
     name: "email",
-    message: "Enter the employee Email:"
+    message: "Enter the Manager Email:"
     },
 {
     type: "input",
     name: "officeNumber",
     message: "What is the manager office number?",
-    when(choices) {
-        return choices.role === "Manager"
+    validate: nameInput => {
+        if (isNaN(nameInput)) {
+            console.log ("Please enter a Number!")
+            return false;
+        }else {
+            return true;
+        }
     }
+}
+])
+
+.then(manInput => {
+    const  { name, id, email, officeNumber } = manInput; 
+    const manager = new Manager (name, id, email, officeNumber);
+
+    teamArray.push(manager); 
+    console.log(manager); 
+})
+};
+
+const empQ = () => {
+ console.log('-- Add Employees to the Team --');
+
+return inquirer.prompt ([
+{
+    type: "list",
+    name: "role",
+    message: "Select the type of Employee:",
+    choices: ["Engineer", "Intern"]
 },
+
+{
+    type: "input",
+    name: "name",
+    message: "Name of Employee?",
+
+},
+
+{
+    type: "input",
+    name: "id",
+    message: "What is the Employee ID?",
+    validate: nameInput => {
+    if (isNaN(nameInput)) {
+        console.log ("Please enter a Number!")
+        return false;
+    }else {
+        return true;
+    }
+  }
+},
+
+{
+    type: "input",
+    name: "email",
+    message: "Enter the Employee Email:"
+    },
 
 {
     type: "input",
@@ -45,7 +107,8 @@ const questions = [{
         return choices.role === "Engineer"
     }
 
-    },
+},
+
 {
         type: "input",
         name: "school",
@@ -54,9 +117,48 @@ const questions = [{
             return choices.role === "Intern"
         }
 
-    },
+},
 
-];
+{
+    type: 'confirm',
+    name: 'confirm',
+    message: 'Would you like to add more team members?',
+    default: false
+}
+])
+
+
+.then(empInput => {
+    const  { name, id, email, role, github, school, confirm } = empInput; 
+    //const employee = new Employee (name, id, email, role, github, school);
+    let employee; 
+
+    if (role === "Engineer") {
+        employee = new Engineer (name, id, email, github);
+
+        console.log(employee);
+
+    } else if (role === "Intern") {
+        employee = new Intern (name, id, email, school);
+
+        console.log(employee);
+    }
+
+    teamArray.push(employee); 
+
+    if (confirm) {
+        return empQ(teamArray); 
+    } else {
+        return teamArray;
+    }
+})
+
+};
+
+
+manQ ()
+.then(empQ)
+
 
 // inquirer
 //   .prompt(questions)
@@ -71,22 +173,22 @@ const questions = [{
 //     }
 // })
 
-// // TODO: Create a function to write HTML file
-function writeToFile(fileName, data) {
-    fs.writeFileSync(fileName, data)
-};
+// // // TODO: Create a function to write HTML file
+// function writeToFile(fileName, data) {
+//     fs.writeFileSync(fileName, data)
+// };
 
-// // // TODO: Create a function to initialize app
-function init() {
-    inquirer.prompt(questions)
-    .then((data) => {
-    let readMeString = new Employee(data)
-    writeToFile ("output/team.html", readMeString)
-    })
-};
+// // // // TODO: Create a function to initialize app
+// function init() {
+//     inquirer.prompt(questions)
+//     .then((data) => {
+//     let newEmployee = new Employee(data)
+//     writeToFile ("output/team.html", newEmployee)
+//     })
+// };
 
-// Function call to initialize app
-init();
+// // Function call to initialize app
+// init();
 
 
 //Gather User input form Inquirer
